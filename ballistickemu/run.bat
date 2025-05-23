e5a0c35d8ee7bd1c29520a088b2da82d44fdcd33
ebubekir-tas <tas_ebubekir@yahoo.com>
2025-05-23 00:22:08 -0400

    update run.bat

--- ballistickemu/run.bat
+++ ballistickemu/run.bat
@@ -1,3 +1,3 @@
 @echo off
-java -jar "target\ballistickemu-0.0.1-SNAPSHOT-jar-with-dependencies.jar"
+java -Xms512m -Xmx1024m -jar "target\ballistickemu-0.0.1-SNAPSHOT-jar-with-dependencies.jar"
 pause
